95f71b4543f726ca55fad2af50610f5463a4d535
Darla Sawler <darlasawler@gmail.com>
2012-12-02 20:40:19 -0500

    add upload script back

---
+++ upload_site.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+source .serverinfo 
+
+echo "Generating site..."
+jekyll
+
+if [[ $? -ne 0 ]]; then
+  echo "Could not generate site!"
+  exit 1
+fi
+
+echo "Uploading to $USER@$HOST:$DESTDIR..."
+rsync -v --progress --compress --recursive --checksum --delete  _site/ $USER@$HOST:$DESTDIR
+
+if [[ $? -eq 0 ]]; then
+  echo "Done!"
+else
+  echo "Errors!"
+fi
